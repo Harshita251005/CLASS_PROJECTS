257e4885af174e9c0afc7ad043dc17c70278558f
Harshita <157570750+Harshita251005@users.noreply.github.com>
2025-12-04 09:27:17 +0530

    Delete career-counseling/career-counseling directory

--- career-counseling/career-counseling/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-// middleware/logger.js
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url} at ${new Date().toISOString()}`);
-  next(); // Move to next middleware/route
-};
-
-module.exports = logger;
